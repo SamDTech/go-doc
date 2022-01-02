744422f67dbeac8e5d91124d656920b118c5e3f8
sam <opeyemisamuel222@gmail.com>
2022-01-02 12:36:57 +0100

    add to file

--- structs.go
+++ structs.go
@@ -1,6 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
 
 type Product struct {
 	id string
@@ -8,6 +14,16 @@ type Product struct {
 	description string
 	price float64
 
+}
+
+func (prod *Product) store(){
+	file, _ := os.Create(prod.id + ".txt")
+
+	content := fmt.Sprintf("ID: %v\nTitle: %v\ndescription: %v\nprice: %.2f\n", prod.id, prod.title, prod.description, prod.price)
+
+	file.WriteString(content)
+
+	file.Close()
 }
 
 func (prod *Product) printData(){
@@ -23,16 +39,41 @@ func NewProduct(id string, t string, d string, p float64) *Product {
 }
 
 func main(){
-	firstProduct := Product {
-		id: "first-product",
-		title: "book",
-		description: "a new book",
-		price: 10.99,
-	}
+	createdProduct := getUserInput()
 
-	secondProduct := NewProduct("second product", "pen", "a new pen", 9.99)
+	createdProduct.printData()
 
-	firstProduct.printData()
+	createdProduct.store()
+}
 
-	secondProduct.printData()
+func getUserInput() *Product{
+	fmt.Println("Please Enter the product details")
+
+	fmt.Println("---------------------------")
+
+	reader := bufio.NewReader(os.Stdin)
+
+	idInput := readUserInput(reader, "Product ID: ")
+
+	titleInput := readUserInput(reader, "Product title: ")
+
+	descInput := readUserInput(reader, "Product description: ")
+
+	priceInput := readUserInput(reader, "Product price: ")
+
+	priceValue, _ := strconv.ParseFloat(priceInput, 64)
+
+	product := NewProduct(idInput, titleInput, descInput, priceValue)
+
+	return product
+}
+
+func readUserInput(reader *bufio.Reader, promptText string) string{
+	fmt.Print(promptText)
+
+	userInput, _ := reader.ReadString('\n')
+
+	userInput = strings.Replace(userInput, "\n", "", -1)
+
+	return userInput
 }

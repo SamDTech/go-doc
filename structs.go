8bc7330a2e2302c6753d211102e6258909a14a60
sam <opeyemisamuel222@gmail.com>
2022-01-02 11:19:27 +0100

    create new instance of the structs

--- structs.go
+++ structs.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 type Product struct {
 	id string
 	title string
@@ -8,6 +10,21 @@ type Product struct {
 
 }
 
-func main(){
+func NewProduct(id string, t string, d string, p float64) *Product {
+		return &Product {id, t, d, p }
 
+}
+
+func main(){
+	firstProduct := Product {
+		id: "first-product",
+		title: "book",
+		description: "a new book",
+		price: 10.99,
+	}
+
+	secondProduct := NewProduct("second product", "pen", "a new pen", 9.99)
+
+	fmt.Println(firstProduct)
+	fmt.Println(*secondProduct)
 }

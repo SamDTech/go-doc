764e32d0e9055483d638e8b94d9fe1511624325b
sam <opeyemisamuel222@gmail.com>
2022-01-02 10:59:53 +0100

    add the product structs

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/samdtech/structs
+
+go 1.17
---
+++ structs.go
@@ -0,0 +1,13 @@
+package main
+
+type Product struct {
+	id string
+	title string
+	description string
+	price float64
+
+}
+
+func main(){
+
+}
